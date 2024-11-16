6a7c1e3ffbfa0fe28478c15b93fc676662912eee
Rizkia Adhy Syahputra <rizkia.as.pac@gmail.com>
2024-11-16 14:22:43 +0900

    completing

--- install.sh
+++ install.sh
@@ -1,1 +1,6 @@
 sudo pacman -S --needed --noconfirm - <./arch-official-packages.txt
+
+sudo mkdir -p $HOME/shell_config_features/ftr-vim
+
+sudo cp -r ./config.sh $HOME/shell_config_features/ftr-vim/config.sh
+sudo cp -r ./.vimrc $HOME/.vimrc
---
+++ uninstall.sh
@@ -0,0 +1,4 @@
+sudo pacman -Rnsu - <./arch-official-packages.txt
+
+sudo rm -r $HOME/shell_config_features/ftr-vim
+sudo rm -r $HOME/.vimrc
